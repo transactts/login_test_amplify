dcc4c10f8c7b4357f290cd9bb42b692b3b2bd0f3
unknown <richard.e@transactts.co.uk>
2020-10-19 15:49:41 +0100

    login test

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-2_eu-west-2_M255NdZQ2', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '67235v9h4s6tsc3m8e7e04cpqm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
